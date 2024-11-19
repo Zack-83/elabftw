9872c8795aadc7f9431f4b0cf78433b9417392a1
MoustaphaCamara <cmr.mous@gmail.com>
2024-11-19 15:57:46 +0100

    wip works but codes a mess

--- src/ts/bookingparams.ts
+++ src/ts/bookingparams.ts
@@ -7,25 +7,30 @@
  * @package elabftw
  */
 document.addEventListener('DOMContentLoaded', () => {
-  if (window.location.pathname !== '/database.php' || !window.location.search.includes('?mode=edit')) {
+  if (window.location.pathname !== '/database.php') {
     return;
   }
 
   const minStartTime = document.getElementById('minStartTime') as HTMLSelectElement;
   const maxEndTime = document.getElementById('maxEndTime') as HTMLSelectElement;
 
-  console.log(minStartTime, maxEndTime);
   // Function to update maxEndTime options based on minStartTime's selected value
   function updateMaxEndTimeOptions() {
     const selectedMinTime = minStartTime.value;
+
+    // update maxEndTime to minTime + 1 hour, as item can't be booked from e.g 7am to 7am
+    const minTimeDate = new Date(`1970-01-01T${selectedMinTime}Z`);
+    minTimeDate.setUTCHours(minTimeDate.getUTCHours() + 1);
+    const nextHour = minTimeDate.toISOString().substring(11, 19); // Extract time in HH:mm:ss format
+
     // Disable each maxEndTime option that's inferior to minStartTime
     Array.from(maxEndTime.options).forEach(option => {
-      option.disabled = option.value < selectedMinTime;
+      option.disabled = option.value < nextHour;
     });
 
     // Update the maxEndTime to never be inferior, if min time is selected later
-    if (maxEndTime.value < selectedMinTime) {
-      maxEndTime.value = selectedMinTime;
+    if (maxEndTime.value < nextHour) {
+      maxEndTime.value = nextHour;
       // When the js updates the maxEndTime, user does not trigger the blur event. Send it here so we prevent having min time > to end time in db.
       const event = new Event('blur');
       maxEndTime.dispatchEvent(event);
@@ -33,5 +38,4 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   minStartTime.addEventListener('change', updateMaxEndTimeOptions);
-  updateMaxEndTimeOptions();
 });
