c59307eacb1d73256d2ab6f43bd32dbe1b58805d
MoustaphaCamara <cmr.mous@gmail.com>
2024-12-04 14:14:23 +0100

    sql

--- src/sql/schema169.sql
+++ src/sql/schema169.sql
@@ -1,4 +1,4 @@
--- schema 168
+-- schema 169
 ALTER TABLE `items` ADD `book_min_start_time` TINYINT UNSIGNED NOT NULL DEFAULT 0 AFTER `book_cancel_minutes`;
 ALTER TABLE `items` ADD `book_max_end_time` TINYINT UNSIGNED NOT NULL DEFAULT 0 AFTER `book_min_start_time`;
-UPDATE config SET conf_value = 168 WHERE conf_name = 'schema';
+UPDATE config SET conf_value = 169 WHERE conf_name = 'schema';
